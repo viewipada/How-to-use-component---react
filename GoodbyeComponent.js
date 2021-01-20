01e8521bef264781e21c185e5ba9d01d8eb71e57
viewipada <60265499+viewipada@users.noreply.github.com>
2021-01-20 23:38:35 +0700

    Create GoodbyeComponent.js

---
+++ GoodbyeComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const GoodbyeComponent =({nickname})=>{
+    return(
+        <div>
+            <h1>Bye Bye, {nickname}</h1>
+        </div>
+    )
+}
+export default GoodbyeComponent;
